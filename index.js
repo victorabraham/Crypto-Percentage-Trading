63001f8e675728a31aea0e304becb04979c19be5
LAPTOP-3Q5U0S4E\victo <manoj.gautam@theathenegroup.com>
2018-01-01 17:16:58 -0700

    Hitting coinmarketcap for data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var request = require('request');
 var moment = require('moment-timezone');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
@@ -37,42 +38,45 @@ var purchaseObj = {};
 
 
 function getAggregates() {
-    fs.readFile('./data/aggregate.json', 'utf-8', function(err,data){
+    request('https://api.coinmarketcap.com/v1/ticker/?limit=100', function (error, response, data) {
         var coinList = JSON.parse(data);
-        coinList.sort(function(a,b){
+        coinList.sort(function (a, b) {
             var a1h = parseFloat(a.percent_change_1h);
             var b1h = parseFloat(b.percent_change_1h);
             return a1h > b1h ? -1 : 1;
         });
         topCoins = coinList;
         let count = 0;
-        for(let i = 0; i< 20; i++) {
-            //topSet.push('BTC-'+topCoins[i].symbol);
-            if(marketArray.indexOf('BTC-'+topCoins[i].symbol) != -1 && 
-                topCoins[i].percent_change_1h > 0.5 && count <= 10 && 
-                (!purchaseObj['BTC-'+topCoins[i].symbol] || purchaseObj['BTC-'+topCoins[i].symbol].totalHoldings == 0)){
-                count ++;
-                console.log('BTC-'+topCoins[i].symbol,'    ',topCoins[i].percent_change_1h,'    ',topCoins[i].percent_change_24h,'   ',topCoins[i].price_btc);
+        for (let i = 0; i < 20; i++) {
+            if (marketArray.indexOf('BTC-' + topCoins[i].symbol) != -1 &&
+                topCoins[i].percent_change_1h > 0.5 && 
+                topCoins[i].percent_change_24h > 1 && count <= 10 &&
+                (!purchaseObj['BTC-' + topCoins[i].symbol] || purchaseObj['BTC-' + topCoins[i].symbol].totalHoldings == 0)) {
+                count++;
+                console.log('BTC-' + topCoins[i].symbol, '    ', topCoins[i].percent_change_1h, '    ', topCoins[i].percent_change_24h, '    ',topCoins[i].rank, '   ', topCoins[i].price_btc);
                 let buyObj = {};
                 buyObj.time = new Date();
-                buyObj.count = 0.1/topCoins[i].price_btc;
+                buyObj.count = 0.1 / topCoins[i].price_btc;
                 buyObj.buyPrice = topCoins[i].price_btc;
-                if(!purchaseObj['BTC-'+topCoins[i].symbol]) {
-                    purchaseObj['BTC-'+topCoins[i].symbol] = {buys: [], sells: []};
+                if (!purchaseObj['BTC-' + topCoins[i].symbol]) {
+                    purchaseObj['BTC-' + topCoins[i].symbol] = {
+                        buys: [],
+                        sells: []
+                    };
                 }
-                if(!purchaseObj['BTC-'+topCoins[i].symbol].buys) {
-                    purchaseObj['BTC-'+topCoins[i].symbol].buys = []; 
+                if (!purchaseObj['BTC-' + topCoins[i].symbol].buys) {
+                    purchaseObj['BTC-' + topCoins[i].symbol].buys = [];
                 }
-                purchaseObj['BTC-'+topCoins[i].symbol].buys.push(buyObj);
+                purchaseObj['BTC-' + topCoins[i].symbol].buys.push(buyObj);
             }
         }
-        Object.keys(purchaseObj).forEach(function(key) {
+        Object.keys(purchaseObj).forEach(function (key) {
             let totalHoldings = 0;
-            
-            purchaseObj[key].buys.forEach(function(buyObj) {
+
+            purchaseObj[key].buys.forEach(function (buyObj) {
                 totalHoldings += buyObj.count;
             });
-            purchaseObj[key].buys.forEach(function(sellObj) {
+            purchaseObj[key].buys.forEach(function (sellObj) {
                 totalHoldings += sellObj.count;
             });
             purchaseObj[key].totalHoldings = totalHoldings;
@@ -81,20 +85,20 @@ function getAggregates() {
 }
 
 let marketArray = [];
+
 function getMarkets() {
-    fs.readFile('./data/markets.json', 'utf-8', function(err,data){
+    fs.readFile('./data/markets.json', 'utf-8', function (err, data) {
         var marketList = JSON.parse(data);
         marketList.forEach(function (market) {
-            if(market.MarketName.indexOf('BTC-') == 0) {
+            if (market.MarketName.indexOf('BTC-') == 0) {
                 marketArray.push(market.MarketName);
-                // console.log(market.MarketName);
-                // if(topSet.indexOf(market.MarketName) != -1){
-                //     console.log(market.MarketName);
-                // }
             }
         });
         getAggregates();
-        setTimeout(function(){getAggregates();}, 2000);
+        setInterval(function () {
+            console.log('#############################')
+            getAggregates();
+        }, 5000);
     });
 }
 
@@ -105,11 +109,11 @@ app.get('/tickers', function (req, res) {
     res.send(TRACKED_COINS);
 });
 
-app.get('/top', function(req, res) {
-    res.send(topCoins.slice(0,10));
+app.get('/top', function (req, res) {
+    res.send(topCoins.slice(0, 10));
 });
 
-app.get('/buys', function(req, res) {
+app.get('/buys', function (req, res) {
     res.send(purchaseObj);
 });
 
